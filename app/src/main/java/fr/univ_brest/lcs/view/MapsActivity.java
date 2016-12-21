00f6567eda932043b351c2a565b81f2c5a0c0e84
FilsOfKro <owenn.pantry@gmail.com>
2016-12-21 03:05:32 +0100

    Added FusedLocation API [WIP]

--- app/src/main/java/fr/univ_brest/lcs/view/MapsActivity.java
+++ app/src/main/java/fr/univ_brest/lcs/view/MapsActivity.java
@@ -1,20 +1,32 @@
 package fr.univ_brest.lcs.view;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import fr.univ_brest.lcs.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import android.util.Log;
+
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GoogleApiClient myGoogleApiClient;
+    private String myLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Create an instance of GoogleAPIClient.
+        if (myGoogleApiClient == null) {
+            myGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
@@ -39,13 +60,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        myGoogleApiClient.connect();
 
-        // Add a marker in Sydney and move the camera
+        // Add a marker in Sydney and UBO
         LatLng sydney = new LatLng(-34, 151);
-        LatLng ubo = new LatLng(54,-4);
+        LatLng ubo = new LatLng(48.5,-4.5);
 
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.addMarker(new MarkerOptions().position(ubo).title("Marker in UBO"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubo));
+
+        //Find user's location
+        Location myLastLocation = LocationServices.FusedLocationApi.getLastLocation(myGoogleApiClient);
+        LatLng me = new LatLng(myLastLocation.getLatitude(), myLastLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(me);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        mCurrLocation = mGoogleMap.addMarker(markerOptions);
+
+        //and move the camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
+    }
+
+    //Requested by ConnectionCallbacks
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        myLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(myLastLocation != null){
+            myLatitude.setText(String.valueOf(myLastLocation.getLatitude()));
+            myLongitude.setText(String.valueOf(myLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
